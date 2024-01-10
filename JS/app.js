56d88b77a51c95dfaa2de413f286c093ef6ae33f
Kalliran <theautumn90@gmail.com>
2024-01-10 09:55:08 -0600

    added pricing component and links to and from homepage

--- JS/app.js
+++ JS/app.js
@@ -15,3 +15,93 @@ buttons.forEach(button => {
         delete activeSlide.dataset.active //removes the attribute
     })
 });
+
+// query selectors
+console.log('hello');
+
+let myCheckbox = document.querySelector('.switch input'),
+
+    duration = document.querySelector('.month'),
+    price = document.querySelector('.price .pricefix'),
+    switcher = document.querySelector('.switch'),
+    range = document.querySelector('.myRange');
+
+    // create price on switch
+    switcher.onclick = function(){
+        if(myCheckbox.checked == true){
+            let priceInt = parseInt(price.textContent);
+            price.textContent = `${(priceInt - (priceInt * .25)) * 12}`; //discount
+
+            duration.textContent = `/year`;
+        }else {
+            updatePrice()
+            duration.textContent = `/month`;
+        }
+    }
+
+    // add selector to page-view
+    let pageViews = document.querySelector('.page-views span');
+
+    // add event listener to input
+    range.addEventListener('input', updatePrice());
+
+    // update price
+function updatePrice(){
+    if(range.value == 1){
+        if(myCheckbox.checked == true){
+            price.textContent = `${(8 - (8 * .25)) * 12}`; // what it will update to if checked
+        }else{
+            price.textContent = 8;
+        }
+
+        pageViews.textContent = '10k';
+    }
+
+    if(range.value == 2){
+        if(myCheckbox.checked == true){
+            price.textContent = 108; //yearly price
+        }else{
+
+            price.textContent = 12; //monthly price
+        }
+
+        pageViews.textContent = '50k';
+
+    }
+
+    if(range.value == 3){
+        if(myCheckbox.checked == true){
+            price.textContent = 144;
+        }else{
+
+            price.textContent = 16;
+        }
+
+        pageViews.textContent = '100k';
+        
+    }
+
+    if(range.value == 4){
+        if(myCheckbox.checked == true){
+            price.textContent = 216;
+        }else{
+
+            price.textContent = 24;
+        }
+
+        pageViews.textContent = '500k';
+        
+    }
+
+    if(range.value == 5){
+        if(myCheckbox.checked == true){
+            price.textContent = 324;
+        }else{
+
+            price.textContent = 36;
+        }
+
+        pageViews.textContent = '1M';
+        
+    }
+}
